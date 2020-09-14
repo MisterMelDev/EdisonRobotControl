c18f943903f5d29f4e698d50167b44bbe12a1915
Melle Moerkerk <melle@mistermel.tech>
2020-09-14 15:12:50 +0200

    Implement WASD/arrow keys control

--- static/main.js
+++ static/main.js
@@ -6,6 +6,8 @@ const connectionErrorMsg = document.getElementById("connection-error-msg");
 const batteryVoltageElement = document.getElementById("battery-voltage");
 const boardTemperatureElement = document.getElementById("board-temperature");
 
+const speedInput = document.getElementById("speed-input");
+
 socket.addEventListener("open", function(event) {
     console.log("WebSocket opened");
     hideConnectionError();
@@ -37,11 +39,66 @@ socket.addEventListener("error", function(event) {
     setConnectionError("A WebSocket error occurred");
 });
 
+let modalActive = false;
+
 function setConnectionError(errorMsg) {
-    connectionErrorModal.style.display = "block";
-    connectionErrorMsg.innerHTML = errorMsg;
+    //connectionErrorModal.style.display = "block";
+    //connectionErrorMsg.innerHTML = errorMsg;
+    //modalActive = true;
 }
 
 function hideConnectionError() {
     connectionErrorModal.style.display = "none";
+    modalActive = false;
+}
+
+let pressedKeys = {};
+window.onkeyup = function(e) {
+    if(!controlKeyCodes.includes(e.keyCode)) {
+        return;
+    }
+
+    pressedKeys[e.keyCode] = false;
+    sendControlPacket();
+};
+
+window.onkeydown = function(e) {
+    if(!controlKeyCodes.includes(e.keyCode)) {
+        return;
+    }
+
+    pressedKeys[e.keyCode] = true;
+    sendControlPacket();
+}
+
+const controlKeyCodes = [87, 38, 65, 37, 83, 40, 68, 39];
+
+function sendControlPacket() {
+    let maxSpeed = speedInput.value;
+    if(!maxSpeed) {
+        speedInput.value = 300;
+        maxSpeed = 300;
+    }
+
+    let speed = 0;
+    let steer = 0;
+
+    if(pressedKeys[87] || pressedKeys[38]) {
+        speed = maxSpeed;
+    } else if(pressedKeys[83] || pressedKeys[40]) {
+        speed = -maxSpeed;
+    }
+
+    if(pressedKeys[68] || pressedKeys[39]) {
+        steer = maxSpeed;
+    } else if(pressedKeys[65] || pressedKeys[37]) {
+        steer = -maxSpeed;
+    }
+
+    let json = {
+        type: "control",
+        speed: speed,
+        steer: steer
+    };
+    socket.send(JSON.stringify(json));
 }
