17f83b1f324f4e448cb0e8bf374fe42ac1e0b0a8
Melle Moerkerk <melle@mistermel.tech>
2020-09-14 14:55:12 +0200

    Improved error modal

--- static/main.js
+++ static/main.js
@@ -1,22 +1,47 @@
 const socket = new WebSocket("ws://127.0.0.1:8888");
+
 const connectionErrorModal = document.getElementById("connection-error-modal");
+const connectionErrorMsg = document.getElementById("connection-error-msg");
+
+const batteryVoltageElement = document.getElementById("battery-voltage");
+const boardTemperatureElement = document.getElementById("board-temperature");
 
 socket.addEventListener("open", function(event) {
     console.log("WebSocket opened");
-    connectionErrorModal.style.display = "none";
+    hideConnectionError();
 });
 
 socket.addEventListener("message", function(event) {
     let json = JSON.parse(event.data);
-    console.log(json);
+    let msgType = json.type;
+
+    if(msgType == "telemetry") {
+        if(!json.isConnected) {
+            setConnectionError("Unable to communicate with motherboard, is it turned on?");
+        } else {
+            hideConnectionError();
+        }
+
+        batteryVoltageElement.innerHTML = json.battVoltage.toFixed(2) + "v";
+        boardTemperatureElement.innerHTML = json.boardTemp.toFixed(1) + " &#8451;";
+    }
 });
 
 socket.addEventListener("close", function(event) {
     console.log("WebSocket closed");
-    connectionErrorModal.style.display = "block";
+    setConnectionError("WebSocket connection was interrupted");
 });
 
 socket.addEventListener("error", function(event) {
     console.log("WebSocket error");
-    connectionErrorModal.style.display = "block";
+    setConnectionError("A WebSocket error occurred");
 });
+
+function setConnectionError(errorMsg) {
+    connectionErrorModal.style.display = "block";
+    connectionErrorMsg.innerHTML = errorMsg;
+}
+
+function hideConnectionError() {
+    connectionErrorModal.style.display = "none";
+}
