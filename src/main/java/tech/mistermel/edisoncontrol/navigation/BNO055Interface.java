3460c50fc5e95d8f25e5ec9c44f903fafc6b4e20
Melle Moerkerk <melle@mistermel.tech>
2020-10-07 18:08:16 +0200

    Add reading of chip id register

--- src/main/java/tech/mistermel/edisoncontrol/navigation/BNO055Interface.java
+++ src/main/java/tech/mistermel/edisoncontrol/navigation/BNO055Interface.java
@@ -15,11 +15,16 @@ public class BNO055Interface implements MagnetometerInterface {
 
 	private static final Logger logger = LoggerFactory.getLogger(BNO055Interface.class);
 	
+	private static final int CHIP_ID = 0xA0;
+	
 	private static final int BUS_NUM = I2CBus.BUS_1;
 	private static final byte DEVICE_ADDR = 0x28;
+	
+	private static final byte CHIP_ID_ADDR = 0x00;
 	private static final byte EULER_REGISTER_ADDR = 0x1A;
 	
 	private I2CDevice device;
+	private boolean isSensorPresent;
 	
 	@Override
 	public void initialize(JSONObject settings) {
@@ -36,6 +41,16 @@ public class BNO055Interface implements MagnetometerInterface {
 	@Override
 	public float getHeading() {
 		try {
+			if(!isSensorPresent) {
+				if(device.read(CHIP_ID_ADDR) == CHIP_ID) {
+					this.isSensorPresent = true;
+					logger.info("BNO055 connected");
+				} else {
+					logger.debug("Sensor is not present");
+					return 0;
+				}
+			}
+			
 			byte[] buffer = new byte[6];
 			device.read(EULER_REGISTER_ADDR, buffer, 0, buffer.length);
 			
