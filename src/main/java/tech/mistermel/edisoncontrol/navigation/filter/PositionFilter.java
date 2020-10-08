9d1bc505ce3e42fa7e78b22c5dc875d83fee513a
Melle Moerkerk <melle@mistermel.tech>
2020-10-08 19:26:27 +0200

    Limit of 2 decimal places

--- src/main/java/tech/mistermel/edisoncontrol/navigation/filter/PositionFilter.java
+++ src/main/java/tech/mistermel/edisoncontrol/navigation/filter/PositionFilter.java
@@ -1,8 +1,11 @@
 package tech.mistermel.edisoncontrol.navigation.filter;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class PositionFilter {
 
-	private static final int NUM_MEASUREMENTS = 5;
+	private static final int NUM_MEASUREMENTS = 3;
 	
 	private RollingAverage averageX, averageY;
 	
@@ -11,13 +14,22 @@ public class PositionFilter {
 		this.averageY = new RollingAverage(NUM_MEASUREMENTS);
 	}
 	
+	private float round(float val, int decimalPlaces) {
+		BigDecimal bdVal = BigDecimal.valueOf(val);
+		bdVal = bdVal.setScale(decimalPlaces, RoundingMode.HALF_UP);
+		return bdVal.floatValue();
+	}
+	
 	public void inputPosition(float x, float y) {
 		averageX.add(x);
 		averageY.add(y);
 	}
 	
 	public float[] getPosition() {
-		return new float[] { averageX.getAverage(), averageY.getAverage() };
+		float x = round(averageX.getAverage(), 2);
+		float y = round(averageY.getAverage(), 2);
+		
+		return new float[] { x, y };
 	}
 	
 }
