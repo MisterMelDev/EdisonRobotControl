659108bbf323988242f7f550b813007e882ec985
Melle Moerkerk <melle@mistermel.tech>
2020-10-16 22:23:12 +0200

    Add util methods to Location

--- src/main/java/tech/mistermel/edisoncontrol/navigation/Location.java
+++ src/main/java/tech/mistermel/edisoncontrol/navigation/Location.java
@@ -12,6 +12,15 @@ public class Location {
 		this.y = y;
 	}
 	
+	public double distanceTo(Location loc2) {
+		return Math.sqrt(Math.pow(x - loc2.getX(), 2) + Math.pow(y - loc2.getY(), 2));
+	}
+	
+	public double headingTo(Location loc2) {
+		double radians = Math.atan2(loc2.getY() - y, loc2.getX() - x);
+		return Math.toDegrees(radians) + 90;
+	}
+	
 	public void setX(float x) {
 		this.x = x;
 	}
